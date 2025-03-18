b732727a66eb1edb56d014913230601d96fc9f84
Krutish-Raval <ksraval710@gmail.com>
2025-03-18 12:21:20 +0530

    Add backend structure with constants, API response/error handling, and database connection

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,63 @@
+import mongoose from 'mongoose';
 import express from 'express'
 import dotenv from 'dotenv';
-dotenv.config();
+import { DB_NAME } from './constants.js';
+import { app } from './app.js'
+import connectDB from './db/index.js';
+dotenv.config({path: './.env'})
 
-
-const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
 })
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+// ;(async () => {
+//    try{
+//       await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//       app.on("error",(e)=>{
+//             console.log("ERROR: ",e);
+//             throw e;
+//       })
+//       app.listen(port, () => {
+//         console.log(`App listening on port ${port}`)
+//       })
+//    }
+//    catch(e){
+//        console.log("ERROR: " ,e);
+//        throw e;
+//    }
+// })();
 
-app.get('/jokes',(req,res)=>{
-    const jokes = [
-        {
-            id: 1,
-            joke: 'Why did the scarecrow win an award? Because he was outstanding in his field.'
-        },
-        {
-            id: 2,
-            joke: 'Why did the tomato turn red? Because it saw the salad dressing!'
-        },
-        {
-            id: 3, 
-            joke: 'What do you call a fake noodle? An impasta!'
-        }
-    ]
-    res.send(jokes)
-}
-)
-app.get('/login',(req,res)=>{
-    res.send('<h2>Login Page</h2>')
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.get('/jokes',(req,res)=>{
+//     const jokes = [
+//         {
+//             id: 1,
+//             joke: 'Why did the scarecrow win an award? Because he was outstanding in his field.'
+//         },
+//         {
+//             id: 2,
+//             joke: 'Why did the tomato turn red? Because it saw the salad dressing!'
+//         },
+//         {
+//             id: 3, 
+//             joke: 'What do you call a fake noodle? An impasta!'
+//         }
+//     ]
+//     res.send(jokes)
+// }
+// )
+// app.get('/login',(req,res)=>{
+//     res.send('<h2>Login Page</h2>')
+// })
