7e826716a8a50e59bb4fc1462b2c74177ebc572f
Krutish-Raval <ksraval710@gmail.com>
2025-03-22 18:41:52 +0530

    Implement user authentication with login and logout functionality, including JWT verification and token management

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,6 +3,22 @@ import { ApiError } from "../utils/ApiError.js";
 import validator from "validator";
 import { User } from "../models/user.models.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
+
+
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try{
+        user= await User.findById(userId);
+        const accessToken = await user.generateAccessToken();
+        const refreshToken = await user.generateRefreshToken();
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave: false});
+        return {accessToken, refreshToken};
+    }
+    catch(err){
+        throw new ApiError(500, "Something went wrong while generating tokens");
+    }
+}
 const registerUser= asyncHandler(async (req,res,next)=>{
     // get user details
     // validation email in correct format and all required non empty
@@ -11,28 +27,24 @@ const registerUser= asyncHandler(async (req,res,next)=>{
     // remove password and refresh token from response
     // check for user creation
 
-    const {name, leetcodeId, email, password} = req.body;
-    console.log("name: ", name);
-    console.log("leetcodeId: ", leetcodeId);
+    const {email, password} = req.body;
 
     if(
-        [name, leetcodeId, email, password].some((field) => field?.trim() === "")
+        [email, password].some((field) => field?.trim() === "")
     ){
         throw new ApiError(400, "All fields are required");
     }
     else if(!validator.isEmail(email)){
         throw new ApiError(400, "Invalid Email format");
     }
-    const existedUser=await User.findOne({
-        $or: [{email},{name}]
-    })
+    const existedUser=await User.findOne(
+         {email}
+    )
     console.log("existedUser: ", existedUser);
     if(existedUser){
         throw new ApiError(402, "User already exists");
     }
     const user =await User.create({
-        name: name.toLowerCase(),
-        leetcodeId,
         email,
         password
     })
@@ -54,4 +66,74 @@ const registerUser= asyncHandler(async (req,res,next)=>{
     // })
 })
 
-export {registerUser}
+const loginUser=asyncHandler(async(req,res,next)=>{
+    // take email and password
+    // check if user exists
+    // check if password or email is correct
+    // generate access token and refresh token
+    // send cookies
+
+    const {email, password} = req.body;
+    if(!email || !password){
+        throw new ApiError(400, "Email and password are required");
+    }
+    const user=await User.findOne({email});
+    if(!user){
+        throw new ApiError(404, "User does not exist");
+    }
+    const isPasswordCorrect = await user.verifyPassword(password); 
+    if(!isPasswordCorrect){
+        throw new ApiError(401, "Incorrect password");
+    }
+    const {accessToken,refreshToken}=await generateAccessTokenAndRefreshToken(user._id);
+
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+    return res.status(200)
+    .cookie("AccessToken", accessToken, options)
+    .cookie("RefreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200,
+        {
+            user:loggedInUser,
+            accessToken,
+            refreshToken
+
+        },
+        "User logged in successfully"
+        )
+    )
+})
+
+const logOutUser=asyncHandler(async(req,res,next)=>{
+    // clear cookies
+
+    await User.findByIdAndUpdate(
+        req.user._id, 
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))  
+})
+export {registerUser,loginUser,logOutUser}
--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -4,4 +4,9 @@ import { registerUser } from '../controllers/user.controller.js';
 const router = Router();
 
 router.route("/register").post(registerUser);
+
+
+router.route("/login").post(loginUser);
+
+router.route("/logout").post(verifyJWT,logOutUser);
 export default router;
