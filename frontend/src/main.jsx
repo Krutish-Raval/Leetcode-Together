f361f111b0c75c363c8e9a512304efee44e71905
Krutish-Raval <ksraval710@gmail.com>
2025-07-25 21:56:35 +0530

    refactor: restructure routing logic into RoutesWrapper component for better readability

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,42 +1,45 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider, useSelector } from "react-redux";
+import { Provider,useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from 'redux-persist/integration/react';
 import App from "./App.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
+import PrivateRoute from './components/PrivateRoute.jsx';
 import "./index.css";
-import Layout from "./Layout.jsx";
-import ContestLists from "./pages/ContestLists.jsx";
-import AddFriends from "./pages/Friend.jsx";
-import FriendStanding from "./pages/FriendStanding.jsx";
 import HomePage from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
-import { persistor, store } from "./store/store.js";
+import { persistor, store } from './store/store.js';
+import Layout from "./Layout.jsx";
+import AddFriends from "./pages/Friend.jsx";
+import ContestLists from "./pages/ContestLists.jsx";
+import FriendStanding from "./pages/FriendStanding.jsx"
 
-const user = useSelector((state) => state.auth.email);
+// const user = useSelector((state) => state.auth.email);
+const RoutesWrapper = () => {
+  const user = useSelector((state) => state.auth.email);
 
+  return (
+    <Routes>
+      <Route path="/" element={user ? <Navigate to="/home" /> : <App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<PrivateRoute />}>
+        <Route element={<Layout />}>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/add-friends" element={<AddFriends />} />
+          <Route path="/contest-lists" element={<ContestLists />} />
+          <Route path="/contest-lists/:contest-name" element={<FriendStanding />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+};
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={user ? <Navigate to="/home" /> : <App />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          /* Protected Routes with Layout */
-          <Route element={<PrivateRoute />}>
-            <Route path="/" element={<Layout />}>
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/add-friends" element={<AddFriends />} />
-              <Route path="/contest-lists" element={<ContestLists />} />
-              <Route
-                path="/contest-lists/:contest-name"
-                element={<FriendStanding />}
-              />
-            </Route>
-          </Route>
-        </Routes>
+        <RoutesWrapper />
       </BrowserRouter>
     </PersistGate>
   </Provider>
