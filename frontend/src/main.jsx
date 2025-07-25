c4b861f0bf2a6f5c1bb83a2cfbb1ad0bd90c7474
Krutish-Raval <ksraval710@gmail.com>
2025-07-25 21:48:20 +0530

    fix: update user check in PrivateRoute to ensure non-empty string

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,36 +1,39 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
-import PrivateRoute from './components/PrivateRoute.jsx';
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import "./index.css";
+import Layout from "./Layout.jsx";
+import ContestLists from "./pages/ContestLists.jsx";
+import AddFriends from "./pages/Friend.jsx";
+import FriendStanding from "./pages/FriendStanding.jsx";
 import HomePage from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
-import { persistor, store } from './store/store.js';
-import Layout from "./Layout.jsx";
-import AddFriends from "./pages/Friend.jsx";
-import ContestLists from "./pages/ContestLists.jsx";
-import FriendStanding from "./pages/FriendStanding.jsx"
+import { persistor, store } from "./store/store.js";
+
+const user = useSelector((state) => state.auth.email);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={user ? <Navigate to="/home" /> : <App />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-
           /* Protected Routes with Layout */
           <Route element={<PrivateRoute />}>
             <Route path="/" element={<Layout />}>
               <Route path="/home" element={<HomePage />} />
-              <Route path="/add-friends" element={<AddFriends/>} />
-              <Route path="/contest-lists" element={<ContestLists/>} />
-              <Route path="/contest-lists/:contest-name" element={<FriendStanding/>}/>
+              <Route path="/add-friends" element={<AddFriends />} />
+              <Route path="/contest-lists" element={<ContestLists />} />
+              <Route
+                path="/contest-lists/:contest-name"
+                element={<FriendStanding />}
+              />
             </Route>
           </Route>
         </Routes>
